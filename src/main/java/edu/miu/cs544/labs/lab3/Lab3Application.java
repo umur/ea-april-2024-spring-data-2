cff2db653bec8e060cf22d043695e85118437475
Mohamed Nabil <mohamedmenasy@gmail.com>
2024-04-04 19:23:45 +0200

    Project Setup

---
+++ src/main/java/edu/miu/cs544/labs/lab3/Lab3Application.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.labs.lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3Application.class, args);
+	}
+
+}

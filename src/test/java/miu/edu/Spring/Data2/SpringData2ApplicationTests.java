ca4fa1762cc458548ee53cbb2ec6a56bd1e75e71
prayash27 <prayash.shrestha@wiseyak.com>
2024-04-04 14:16:32 -0500

    initialized the project

---
+++ src/test/java/miu/edu/Spring/Data2/SpringData2ApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.Spring.Data2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringData2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

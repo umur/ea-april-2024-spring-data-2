200f27ced47128a87082645f5bad50c80f547218
duy.truong <si.truong@miu.edu>
2024-04-14 22:28:38 -0500

    [FEATURE] FetchMode Join

--- src/main/java/spring/data/lab3/entity/Product.java
+++ src/main/java/spring/data/lab3/entity/Product.java
@@ -2,6 +2,7 @@ package spring.data.lab3.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.BatchSize;
@@ -10,9 +11,9 @@ import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @Entity
+@NamedEntityGraph(name = "Product.reviews", attributeNodes = @NamedAttributeNode("reviews"))
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,10 +41,15 @@ public class Product {
 //    @BatchSize(size = 100)
 //    private List<Review> reviews;
 
-    // fetch strategy: EAGER and BATCH
-    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
-    @JsonManagedReference
+//    // fetch strategy: EAGER and BATCH
+//    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
+//    @JsonManagedReference
+//    @Fetch(FetchMode.SELECT)
+//    @BatchSize(size = 100)
+//    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "product", fetch = FetchType.EAGER)
     @Fetch(FetchMode.SELECT)
-    @BatchSize(size = 100)
+    @JsonManagedReference
     private List<Review> reviews;
 }
